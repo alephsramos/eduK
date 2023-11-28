ae60911a92db331e97af0e4386b99c41b2e0a230
aleph ramos <alephramos142857@gmail.com>
2023-11-28 18:18:41 -0300

    sad

--- js/index.js
+++ js/index.js
@@ -1,24 +1,4 @@
-function scrollToSection(sectionId) {
-    const section = document.getElementById(sectionId);
-    if (section) {
-      section.scrollIntoView({
-        behavior: 'smooth'
-      });
-    }
-  }
-  
-  // Adiciona a função de rolagem suave para todos os links que têm um atributo href começando com "#"
-  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-
-      const targetId = this.getAttribute('href').substring(1);
-      const targetSection = document.getElementById(targetId);
-
-      if (targetSection) {
-        targetSection.scrollIntoView({
-          behavior: 'smooth'
-        });
-      }
-    });
-  });
+function toggleMenu() {
+  var dropdownContent = document.querySelector('.dropdown-content');
+  dropdownContent.classList.toggle('show');
+}
